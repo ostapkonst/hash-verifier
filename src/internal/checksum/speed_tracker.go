e1458f338ccf25eed83fa843ce1eefbe5262fec8
Ostap Konstantinov <ostap.konstantinov@yandex.ru>
2026-03-06 13:25:13 +0300

    Changed display from instantaneous time to average time

--- src/internal/checksum/speed_tracker.go
+++ src/internal/checksum/speed_tracker.go
@@ -1,61 +1,52 @@
 package checksum
 
 import (
-	"math/big"
 	"sync"
 	"time"
 )
 
-const speedUpdateInterval = 1.5
-
 type SpeedTracker struct {
-	rwm        sync.RWMutex
-	totalBytes big.Int
-	lastBytes  big.Int
-	lastTime   time.Time
-	lastSpeed  float64
+	mu        sync.Mutex
+	bytes     int64
+	startTime time.Time
+	speed     float64
 }
 
 func NewSpeedTracker() *SpeedTracker {
-	return &SpeedTracker{
-		totalBytes: *big.NewInt(0),
-		lastBytes:  *big.NewInt(0),
-		lastTime:   time.Now(),
-	}
+	return &SpeedTracker{}
 }
 
 func (t *SpeedTracker) AddBytes(n int64) {
-	t.rwm.Lock()
-	defer t.rwm.Unlock()
+	t.mu.Lock()
+	defer t.mu.Unlock()
 
-	t.totalBytes.Add(&t.totalBytes, big.NewInt(n))
+	t.bytes += n
 
-	now := time.Now()
-	elapsed := now.Sub(t.lastTime).Seconds()
-
-	if elapsed >= speedUpdateInterval {
-		bytesDelta := new(big.Int).Sub(&t.totalBytes, &t.lastBytes)
-
-		t.lastBytes.Set(&t.totalBytes)
-		t.lastTime = now
-
-		t.lastSpeed = float64(bytesDelta.Int64()) / elapsed
+	if t.startTime.IsZero() {
+		t.startTime = time.Now()
+		return
 	}
+
+	elapsed := time.Since(t.startTime).Seconds()
+	if !(elapsed > 0) {
+		return
+	}
+
+	t.speed = float64(t.bytes) / elapsed
 }
 
 func (t *SpeedTracker) Speed() float64 {
-	t.rwm.RLock()
-	defer t.rwm.RUnlock()
+	t.mu.Lock()
+	defer t.mu.Unlock()
 
-	return t.lastSpeed
+	return t.speed
 }
 
 func (t *SpeedTracker) Reset() {
-	t.rwm.Lock()
-	defer t.rwm.Unlock()
+	t.mu.Lock()
+	defer t.mu.Unlock()
 
-	t.totalBytes.SetInt64(0)
-	t.lastBytes.SetInt64(0)
-	t.lastSpeed = 0
-	t.lastTime = time.Now()
+	t.bytes = 0
+	t.speed = 0
+	t.startTime = time.Time{}
 }
